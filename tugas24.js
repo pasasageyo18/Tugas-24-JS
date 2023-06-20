d934e3ac2ad474da6a9e6a4e615a3d0893faa609
Muhammad Nur Faza <gofar222@gmail.com>
2023-06-20 09:30:23 +0700

    second commit

--- tugas24.js
+++ tugas24.js
@@ -1,4 +1,5 @@
 let x = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
 let y = x.sort();
-console.log(y);
-console.log(y.reverse());
+console.log("Sebelumnya :", x);
+console.log("Ascending :", y);
+console.log("Descending :", y.reverse());
